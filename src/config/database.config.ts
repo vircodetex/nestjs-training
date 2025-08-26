8abb7ad37e620e3a640e660d04bc51904c372172
Virginie <virginie@texafe.com>
2025-08-26 16:29:04 +0200

    misc: more database config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -13,4 +13,7 @@ export const typeOrmConfig = registerAs("database", (): TypeOrmModuleOptions => 
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     synchronize: Boolean(process.env.DB_SYNC ?? false),
+    ssl: {
+        rejectUnauthorized: false,
+    },
 }));
